c57e01c37b32c769b2852a6eb9532671752cf1a8
Minh Bui <68634312+devilblood195@users.noreply.github.com>
2020-09-26 10:27:02 +0700

    Add files via upload

---
+++ home/scripts/index.js
@@ -0,0 +1,13 @@
+/* Toogle Search */
+$(document).ready(function() {
+    $(".search-toggle").click(function() {
+        $(".sear-toggle").toggle(200);
+        $(":disabled").css(".search-toggle");
+    });
+});
+
+$(document).ready(function() {
+    $(".btn-exit").click(function() {
+        $(".sear-toggle").toggle(200);
+    });
+});
