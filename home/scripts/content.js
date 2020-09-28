a055543596fb9408d35bcfc5e10b63466d6b99e9
hienkcxd <70784911+hienkcxd@users.noreply.github.com>
2020-09-28 12:11:54 +0700

    Add files via upload

---
+++ home/scripts/content.js
@@ -0,0 +1,10 @@
+
+
+function show()
+{
+    var title1 = document.getElementsByClassName("H_name")[0].innerHTML;
+    var title2 = document.getElementsByClassName("H_name_tag")[0].innerHTML;
+
+    document.getElementsByClassName("H_title_1")[0].innerHTML = title1;
+    document.getElementsByClassName("H_title_2")[0].innerHTML = title2;
+}
